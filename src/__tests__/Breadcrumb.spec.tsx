f81835dc2e11b54819c2bc7421e2b20bf5604557
jpmonette <contact@jpmonette.net>
2018-08-12 21:51:03 +0100

    Update test coverage

--- src/__tests__/Breadcrumb.spec.tsx
+++ src/__tests__/Breadcrumb.spec.tsx
@@ -5,17 +5,29 @@ import { Breadcrumb } from "../";
 
 describe("Breadcrumb", () => {
   it("should generate an empty breadcrumb", () => {
-    const tree = create(<Breadcrumb />).toJSON();
+    const tree = create(
+      <Breadcrumb
+        hasArrowSeperator={true}
+        hasBulletSeperator={true}
+        hasDotSeperator={true}
+        hasSucceedsSeparator={true}
+        isCentered={true}
+        isLarge={true}
+        isMedium={true}
+        isRight={true}
+        isSmall={true}
+      />,
+    ).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
 
   it("should generate a breadcrumb", () => {
     const children = [
-      <Breadcrumb.Item key={0} href={"/home"} isActive={true}>
+      <Breadcrumb.Item key={0} href={"/home"}>
         Home
       </Breadcrumb.Item>,
-      <Breadcrumb.Item key={1} isActive={true}>
+      <Breadcrumb.Item key={1} className={"about"} isActive={true}>
         About
       </Breadcrumb.Item>,
     ];
