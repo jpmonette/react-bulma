8b9272e43bdd26acb0bf22284921da604ac23115
jpmonette <contact@jpmonette.net>
2018-08-12 15:25:57 +0100

    Add Container component

---
+++ src/Container.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import classNames from "classnames";
+
+interface Props {
+  className?: string;
+  children?: React.ReactNode;
+  isFluid?: boolean;
+  isWidescreen?: boolean;
+  isFullHD?: boolean;
+}
+
+class Container extends React.Component<Props> {
+  render() {
+    const { children, className, isFluid, isWidescreen, isFullHD, ...props } = this.props;
+
+    return (
+      <div
+        {...props}
+        className={classNames(
+          "container",
+          className,
+          isFluid && "is-fluid",
+          isWidescreen && "is-widescreen",
+          isFullHD && "is-fullhd"
+        )}
+      >
+        {children}
+      </div>
+    );
+  }
+}
+
+export { Container };
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
-import Breadcrumb from "./Breadcrumb";
+import { Breadcrumb, BreadcrumbItem } from "./Breadcrumb";
+import { Container } from "./Container";
 
-export { Breadcrumb };
+export { Breadcrumb, BreadcrumbItem, Container };
